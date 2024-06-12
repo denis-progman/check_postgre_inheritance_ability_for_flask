5db7bc387037751c77ace41b52695057e125c217
Anton <panton160786@gmail.com>
2024-06-12 15:38:20 +0930

    features:google auth from backend

--- app/app.py
+++ app/app.py
@@ -2,11 +2,15 @@ from db import db, migrate
 from flask import Flask
 from configs import current_config 
 from router import routs_init
+from flask_session import Session
 
 app_instance = Flask(__name__)
 app_instance.config.from_object(current_config)
 db.init_app(app_instance)
 migrate.init_app(app_instance, db)
+
+app_instance.config["SESSION_TYPE"] = "filesystem"
+Session(app_instance)
 
 if __name__ == "__main__":
     app_instance.run()
